3deeee0eb61fba92daa830b7a74384d6d6eebd7e
Marina <My@Marina.local>
2015-02-23 01:07:06 +0300

    First response image. Dirty code.

--- httpd.go
+++ httpd.go
@@ -12,13 +12,12 @@ func main() {
 
     ln, err := net.Listen("tcp", ":8888")
     if err != nil {
-        fmt.Println("Error listen")
+        fmt.Println("Error listen: ", err)
     }
-
     for {
         conn, err := ln.Accept()
         if err != nil {
-            fmt.Println("Error accept")
+            fmt.Println("Error accept: ", err)
         }
 
         go handleConnection(conn)
@@ -29,38 +28,44 @@ func handleConnection(conn net.Conn) {
     defer conn.Close()
 
     var buf = make ([]byte, 1024)
-
-    // var buffer []byte
-    // buffer, err := ioutil.ReadAll(conn)
-    // if err != nil {
-    //   fmt.Println("Error readAll")
-    // }
-
     conn.Read(buf)
-
-    // fmt.Println("Buffer: ", string(buf))
 
     header := strings.Split(string(buf), "\n")
     fmt.Println(header[0])
     request := strings.Split(header[0], " ")
 
 
-    if request[0] == "GET" {
-	    fmt.Println(request[1])
-
+    if request[0] == "GET" || request[0] == "HEAD" {
 	    if request[1] == "/" {
 
 	    	file, err := ioutil.ReadFile("index.html")
 			if err != nil {
-		        fmt.Println("Error readFile")
+		        fmt.Println("Error readFile: ", err)
 		    }
 
 			var response_header string = "HTTP/1.1 200 OK\nContent-Type: text/html\nDate: Sun, 22 Feb 2015 20:40:57 GMT\nContent-Length:" + string(len(string(file))) + "\n\n"
 	    	_, errr := conn.Write([]byte(response_header + string(file)))
 			if errr != nil {
-		        fmt.Println("Error write")
+		        fmt.Println("Error write: ", errr)
 		    }
-	    }
+	    } else {
+            var path string = strings.TrimPrefix(request[1], "/")
+            fmt.Println(path)
+            
+            file, err := ioutil.ReadFile(string(path))
+            if err != nil {
+                fmt.Println("Error readFile: ", err)
+            }
+
+            var response_header string = "HTTP/1.1 200 OK\nContent-Type: image/jpg\nDate: Sun, 22 Feb 2015 20:40:57 GMT\nContent-Length:" + string(len(string(file))) + "\n\n"
+            _, errr := conn.Write([]byte(response_header + string(file)))
+            if errr != nil {
+                fmt.Println("Error write: ", errr)
+            }
+        }
+
+
+        
 	}
 }
 
