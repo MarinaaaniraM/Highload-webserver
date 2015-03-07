5e54e6611288905783c1d4980ece6257aa06141a
Marina <My@Marina.local>
2015-03-08 02:10:15 +0300

    Logging. (Dirty code).

---
+++ src/conf/conf.go
@@ -0,0 +1,10 @@
+package conf
+
+ddd string = "12345"
+
+type Day string
+const (
+	a = "111"
+	b = "222"
+	c = "333"
+)
--- src/httpd.go
+++ src/httpd.go
@@ -1,27 +1,38 @@
 package main
 
-import "net"
-import "fmt"
-import "strings"
-import "io/ioutil"
-import "runtime"
-import "os"
-import "strconv"
-import "parser"
-import "net/url"
+import ( 
+    "net"
+    "fmt"
+    "strings"
+    "io/ioutil"
+    "runtime"
+    "os"
+    "strconv"
+    "parser"
+    "net/url"
+    "time"
+)
+// import "conf"
 
 var rootDir string = "."
 var ncpu int = 1
+var isLogging bool =  false
 
 func main() {
     i := 2
     for i <= len(os.Args) {
         if i == len(os.Args) {
-            fmt.Println("________________________________________")
-            fmt.Println("\n'-r /path/to/dir' path to root directory\n'-c 2'            num of CPU\n")
-            fmt.Println("Or run without arguments in defalt\n")
-            fmt.Println("________________________________________")
-            os.Exit(0)
+            switch os.Args[i - 1] {
+            case "-l":
+                isLogging = true
+                break
+            default:
+                fmt.Println("________________________________________")
+                fmt.Println("\n'-r /path/to/dir' path to root directory\n'-c 2'            num of CPU\n")
+                fmt.Println("Or run without arguments in defalt\n")
+                fmt.Println("________________________________________")
+                os.Exit(0)
+            }
         }
         switch os.Args[i - 1] {
         case "-r": 
@@ -29,7 +40,7 @@ func main() {
         case "-c":
             i, er := strconv.Atoi(os.Args[i])
             if er != nil {
-                fmt.Println("Error strconv: ", er)
+                logging("Error strconv: " + er.Error())
             }
             ncpu = i
         }
@@ -38,49 +49,44 @@ func main() {
 
     runtime.GOMAXPROCS(ncpu)
 
-    fmt.Println("\n")
-    fmt.Println("Launching server...")
+    logging("\n")
+    logging("Launching server...")
 
-    ln, err := net.Listen("tcp", ":8080")
+    ln, err := net.Listen("tcp", ":8080")                                       //TODO
     if err != nil {
-        fmt.Println("Error listen: ", err)
+        logging("Error listen: " + err.Error())
     }
     for {
         conn, err := ln.Accept()
         if err != nil {
-            fmt.Println("Error accept: ", err)
+            logging("Error accept: " + err.Error())
         }
         go handleConnection(conn)
     }
 }
 
 func handleConnection(conn net.Conn) {
-    runtime.Gosched()
     defer conn.Close()
-
+    // runtime.Gosched()
+    
     var buf = make ([]byte, 1024)
     conn.Read(buf)
 
     header := strings.Split(string(buf), "\n")
     header = strings.Split(header[0], " ")
     var path string
-    var responseStatus string = "200 OK"
+    var responseStatus string = "200 OK"                                        //TODO
     var fileString string
 
-    fmt.Println("Header: ", header)
+    // fmt.Println("Header: ", header)
 
     if header[0] == "GET" || header[0] == "HEAD" {
 
         url, err := url.Parse(header[1])
         if err != nil {
-            fmt.Println("Error parse url")
+            logging("Error parse url")
         }
-
-        fmt.Println("Request: " + url.Path)
-
-
-        //TODO date!!!
-        
+        logging("Request: " + url.Path)
 
         if strings.Count(url.Path, "../") > strings.Count(rootDir, "/") {
             responseStatus = "403 Forbidden"                                    //TODO
@@ -90,7 +96,7 @@ func handleConnection(conn net.Conn) {
 
             file, err := ioutil.ReadFile(path)
             if err != nil {
-                fmt.Println("Error readFile: ", err)
+                logging("Error readFile: " + err.Error())
                 responseStatus = "403 Forbidden"                                //TODO
             }
             fileString = string(file)
@@ -100,34 +106,42 @@ func handleConnection(conn net.Conn) {
 
             file, err := ioutil.ReadFile(path)
             if err != nil {
-                fmt.Println("Error readFile: ", err)
+                logging("Error readFile: " + err.Error())
                 responseStatus = "404 Not Found"                                //TODO
             }
             fileString = string(file)
         }
-        fmt.Println("Path: " + path)
+        logging("Path: " + path)
 
 	} else {
         responseStatus = "405 Method Not Allowed"                               //TODO
     }
 
-    var httpState string = "HTTP/1.1 " + responseStatus + "\r\n"
+    var httpState string = "HTTP/1.1 " + responseStatus + "\r\n"                //TODO
     var httpContentType string = "Content-Type: " + parser.GetContentType(path) + "\r\n"
     var httpContentLength string = "Content-Length: " + strconv.Itoa(len(fileString)) + "\r\n"
+    var currentDate string = "Date: " + time.Now().Format(time.RFC850) + "\r\n"
     var serverName string = "Server: Cheburashka_v0.1\r\n"
 
-    var responseHeader string = httpState + httpContentType + httpContentLength + serverName + "\r\n"
-    fmt.Println("Response header: \r\n", responseHeader)
+    var responseHeader string = httpState + httpContentType + httpContentLength + currentDate + serverName + "\r\n"
+    // fmt.Println("Response header: \r\n", responseHeader)
 
     if header[0] == "GET" {
         responseHeader = responseHeader + fileString
     }
     _, errr := conn.Write([]byte(responseHeader))
     if errr != nil {
-        fmt.Println("Error write: ", errr)
+        logging("Error write: " + errr.Error())
     }
 
-    fmt.Println("\n")
+    logging("\n")
+}
+
+
+func logging(str string) {
+    if isLogging {
+        fmt.Println(str)
+    }
 }
 
 
